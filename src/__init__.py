54c574c7589dbffc3050f806ca96bba8dfedb013
Jonatan Medina <136943363+jonatanmedina12@users.noreply.github.com>
2024-09-03 18:28:23 -0500

    V1.0.1
    V1.0.1

--- src/__init__.py
+++ src/__init__.py
@@ -2,7 +2,7 @@ from src.controller.analysis_data import AnalysisData
 
 
 def execute():
-    path="D:\\Repositorios\\Project-Python-AnalysisData\\OCRA_raw (3).csv"
+    path=""
     exec_=AnalysisData(path)
     exec_.procesamiento()
     exec_.prueba_normalidad()
